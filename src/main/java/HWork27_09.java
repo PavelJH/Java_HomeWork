95052dffb46472f60547af47c9dea0277de239a1
Kelbas Pavlo <114227096+PavelJH@users.noreply.github.com>
2022-09-28 20:56:31 +0200

    Add files via upload

---
+++ src/main/java/HWork27_09.java
@@ -0,0 +1,116 @@
+public class HWork27_09 {
+    public static void main (String[] args){
+        int test = 0x478;
+
+//        0x478 = 4*16^2+7*16^1+8*16^0 = 1024+112+8 = 1144
+
+        System.out.println(0x478 / 10);
+        System.out.println(0x478 % 10);
+
+        System.out.println();
+
+        System.out.println(0x114 / 10);
+        System.out.println(0x4 % 10);
+
+        System.out.println();
+
+        System.out.println(0x27 / 16);
+        System.out.println(0x7 % 10);
+
+
+//        200345 = 2*10^5+2*10^4+2*10^3+2*10^2+2*10^1+2*10^0
+
+        System.out.println();
+
+//        int test3 = 637; // Добрый вечер, я к сожалению не разобрался, как перевести в шестнадцатеричную (так как там присутствуют буквы) 
+
+
+
+        System.out.println();
+
+        System.out.println(637 / 2);
+        System.out.println(637 % 2);
+
+        System.out.println();
+
+        System.out.println(318 / 2);
+        System.out.println(38 % 2);
+
+        System.out.println();
+
+        System.out.println(159 / 2);
+        System.out.println(159 % 2);
+
+        System.out.println();
+
+        System.out.println(79 / 2);
+        System.out.println(79 % 2);
+
+        System.out.println();
+
+        System.out.println(39 / 2);
+        System.out.println(39 % 2);
+
+        System.out.println();
+
+        System.out.println(19 / 2);
+        System.out.println(19 % 2);
+
+        System.out.println();
+
+        System.out.println(9 / 2);
+        System.out.println(9 % 2);
+
+        System.out.println();
+
+        System.out.println(4 / 2);
+        System.out.println(4 % 2);
+
+        System.out.println();
+
+        System.out.println(2 / 2);
+        System.out.println(2 % 2);
+
+        // 11100111 = 1*2^7+1*2^6+1*2^5+0*2^4+0*2^3+1*2^2+1*2^1+1*2^0 = 231
+
+        int test5 = 637;
+
+        System.out.println();
+
+        System.out.println(637 / 3);
+        System.out.println(637 % 3);
+
+        System.out.println();
+
+        System.out.println(212 / 3);
+        System.out.println(212 % 3);
+
+        System.out.println();
+
+        System.out.println(70 / 3);
+        System.out.println(70 % 3);
+
+        System.out.println();
+
+        System.out.println(23 / 3);
+        System.out.println(23 % 3);
+
+        System.out.println();
+
+        System.out.println(7 / 3);
+        System.out.println(7 % 3);
+
+        System.out.println();
+
+        System.out.println(2 / 3);
+        System.out.println(2 % 3);
+
+
+
+
+
+
+
+
+    }
+}
